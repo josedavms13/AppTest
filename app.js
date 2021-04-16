11f275e546cd5e50543159d6d67efe61ba08b980
josedavms13 <josedav0ms13@gmail.com>
2021-04-15 22:05:57 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const users = [
+    {
+        nombre : 'Pablo',
+        id: 1,
+        Email: 'pablo@gmail.com',
+        NumDogs: 2,
+        Dogs: [ 'Perro A', 'Perro B'
+        ]
+    }
+]
+
+
+
+//#region Form Behavior
+
+function ShowNameFormInputOnNumberOfDogs(){
+    console.log('se cambio')
+}
+
+
+
+
+//#endregion
