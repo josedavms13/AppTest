a273dc0efbcdbff4c43d7d6adb1531404ddfe4d0
josedavms13 <josedav0ms13@gmail.com>
2021-04-16 14:27:57 -0500

    Forms Done

--- app.js
+++ app.js
@@ -10,14 +10,118 @@ const users = [
 ]
 
 
+// ======================== SHOW/HIDE GENERAL FUNCTIONS ==================
 
-//#region Form Behavior
+//#region Show-Hide Forms
 
-function ShowNameFormInputOnNumberOfDogs(){
-    console.log('se cambio')
+
+function OpenForm(id){
+
+    //Id passed in HTML
+
+    document.getElementById(id).classList.remove('d-none');
+
+}
+
+function CloseForm(id){
+    document.getElementById(id).classList.add('d-none');
+}
+
+
+//#endregion
+
+
+
+//========================= SET NAME OF EACH DOG TEXT INPUTS =================
+
+//#region Form Number of Dogs
+
+function getNumbOfDogs(){
+
+    const NumbOfDogs = document.getElementById('dog-number').value;
+
+    generateInputs(NumbOfDogs);
+
+
+    return NumbOfDogs;
+    // console.log(NumbOfDogs);
+
+}
+
+function generateInputs(number){
+    const numbOfInputsContainer = document.getElementById('dog-names-container');
+    // console.log(numbOfInputsContainer)
+    let toPrint = ''
+
+    if(number > 0){
+        toPrint += `
+        <div class="tittle">
+            <h4> Ingresa el nombre de los perros </h4>
+        </div>
+    `
+    }
+    for(let i = 0 ; i < number; i++){
+        toPrint += `
+        <div class="m-2"
+        <label for="dog1 class="display-block">Nombre</label>
+        <input type="text class="display-block" name="dogname" id="d${i}"><br>      
+        </div>
+        `
+    }
+
+
+    // console.log(toPrint)
+
+    numbOfInputsContainer.innerHTML = toPrint;
+
+}
+
+function resetAddDogsNameForm(){
+
+    document.getElementById('dog-number').value = 0;
+    generateInputs();
+
+
+}
+
+function Agregate(){
+    getNamesFromImputs('dog-name-input');
+    resetAddDogsNameForm();
+}
+
+//#endregion
+
+
+
+
+
+function getNamesFromImputs(htmlClass){
+
+
+    const dogNames = [];
+    const ForLoopLimit = getNumbOfDogs();
+    console.log(ForLoopLimit);
+
+    // const i = 1;
+
+
+    for (let i = 0; i < ForLoopLimit; i++) {
+
+        const id = `d${i}`
+        const name = document.getElementById(id).value;
+        
+        console.log(name);
+
+    }
+
+
+    // console.log(dogNames);
+
+
 }
 
 
 
 
-//#endregion
+
+
